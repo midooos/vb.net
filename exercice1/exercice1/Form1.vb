342f6efd4dda6d49f45b09d798acd011547f89ff
Mohamed Dridi <125401114+midooos@users.noreply.github.com>
2025-11-18 18:14:26 +0100

    Add files via upload

---
+++ exercice1/exercice1/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim nom As String
+        Dim age As Integer
+        nom = InputBox("Entrez votre nom")
+        age = InputBox("Entrez votre age")
+        MessageBox.Show("Bonjoour " & nom & " vous avez " & age & " ans")
+    End Sub
+End Class
