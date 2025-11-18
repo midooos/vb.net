b5d677e9962eee3c6a61eb5f60e4be3ded1c01e7
Mohamed Dridi <125401114+midooos@users.noreply.github.com>
2025-11-18 18:15:47 +0100

    Add files via upload

---
+++ exercice3/exercice3/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim a As Integer
+        a = InputBox("donnez votre moyenne")
+        If a >= 10 Then
+            MessageBox.Show("admis")
+        ElseIf 8 <= a <= 10 Then
+            MessageBox.Show("rattrapage")
+        Else
+            MessageBox.Show("ajourné")
+
+        End If
+    End Sub
+End Class
