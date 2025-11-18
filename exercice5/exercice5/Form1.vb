29e0c646c0a67e2875f314caad33ea75607e8f73
Mohamed Dridi <125401114+midooos@users.noreply.github.com>
2025-11-18 18:17:06 +0100

    Add files via upload

---
+++ exercice5/exercice5/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Function carre(x As Integer) As Integer
+        Return x * x
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim a As Integer
+        a = InputBox("entrez un entier")
+        MessageBox.Show("le racine carre est " & carre(a))
+    End Sub
+End Class
